3e9221af3d66a352c4db3b90fd84ce338c9f6cea
Kunlapath <kunlapath.paengsa@gmail.com>
2022-11-28 14:21:05 +0700

    Add project files.

---
+++ RawStringLiterals/Program.cs
@@ -0,0 +1,14 @@
+﻿// https://twitter.com/okyrylchuk/status/1527003752187707395
+
+string name = "Oleg", surname = "Kyrylchuk", test = "5";
+
+string jsonString = 
+    $$"""
+    {
+        "Name": {{name}},
+        "Surname": {{surname}},
+        "Test" : {{test}}
+    }
+    """;
+
+Console.WriteLine(jsonString);
